55236d0fe8487971abf58e9832408e7074b5910c
HimanshuLimbachiya <hlimbachiya90@gmail.com>
2019-01-01 12:00:54 +0530

    commit

---
+++ app/src/main/java/com/example/win10/nointernet/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.win10.nointernet;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements ConnectivityReceiver.ConnectivityReceiverListener {
+
+    TextView textView, type;
+    LinearLayout compat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        compat = findViewById(R.id.llMain);
+        textView = findViewById(R.id.textView);
+        type = findViewById(R.id.tv_Net_Type);
+
+        checkConnection();
+
+        ConnectivityReceiver connectivityReceiver = new ConnectivityReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(connectivityReceiver,intentFilter);
+
+    }
+    private void checkConnection() {
+        boolean isConnected = ConnectivityReceiver.isConnected();
+        showSnack(isConnected);
+    }
+    private void showSnack(boolean isConnected) {
+        if (isConnected) {
+            setContentView(R.layout.activity_main);
+           Toast.makeText(this,"Is Connected",Toast.LENGTH_LONG).show();
+        } else {
+            setContentView(R.layout.internet_screen);
+            Toast.makeText(this,"Is DisConnected",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MyApplication.getInstance().setConnectivityListener(this);
+    }
+
+    @Override
+    public void onNetworkConnectionChanged(boolean isConnected) {
+        showSnack(isConnected);
+    }
+}
